260f2281ca65e76df654b1d063754b3f661b67c5
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-02-22 23:05:19 -0500

    pushing up latest, didn't change the branch

--- server.js
+++ server.js
@@ -7,38 +7,74 @@ const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection(
     {
-        host:"localhost",
+        host: "localhost",
         user: 'root',
-        password:'sqlpass',
-        database:'ETracker_db'
+        password: 'sqlpass',
+        database: 'ETracker_db'
     },
 );
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'toDo',
-            message: 'What would you like to do?:',
-            choices: [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add and employee',
-                'Update an employee role',
-            ],
-        }
-    ])
-    // .then((answers) => {
-    //     console.log ('choice made')
-    // });
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'toDo',
+                message: 'What would you like to do?:',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit',
+                ],
+            }
+        ])
+        .then((answers) => {
+            switch (answers.toDo) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                // case 'View all roles':
+                //     viewRoles();
+                //     break;
+                // case 'View all employees':
+                //     viewEmployees();
+                //     break;
+                // case 'Add a department':
+                //     addDepartment();
+                //     break;
+                // case 'Add a role':
+                //     addRole();
+                //     break;
+                // case 'Add an employee':
+                //     addEmployee();
+                //     break;
+                // case 'Update an employee role':
+                //     addEmployeeRole();
+                //     break;
 
-    app.use((req,res) => {
-        res.status(404).end();
-    })
+            }
+        });
+}
 
-    app.listen(PORT, () => {
-        console.log(`server running on port ${PORT}`);
+// view all departments
+function viewDepartments() {
+    const select = 'SELECT * FROM department';
+    db.query(select,(err,res) => {
+        console.table(res);
+        if (err) throw err;
+        start();
     });
+}
+
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
