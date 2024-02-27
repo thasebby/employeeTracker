d3abc86727a3bf88b195e35cf56b095263b1d810
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-02-27 15:08:54 -0500

    adding the add roles and add department section

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function start() {
 // view all departments
 function viewDepartments() {
     const select = 'SELECT * FROM department';
-    db.query(select,(err,res) => {
+    db.query(select, (err, res) => {
         console.table(res);
         if (err) throw err;
         start();
@@ -79,7 +79,7 @@ function viewDepartments() {
 //view all roles 
 function viewRoles() {
     const select = 'SELECT workRole.role_name,workRole.id,department.name,workRole.role_salary FROM workRole JOIN department ON workRole.department_id = department.id';
-    db.query(select,(err,res) => {
+    db.query(select, (err, res) => {
         console.table(res);
         if (err) throw err;
         start();
@@ -88,8 +88,8 @@ function viewRoles() {
 
 //view all employees
 function viewEmployees() {
-    const select ='SELECT employee.id,employee.first_name,employee.last_name,workRole.role_name,department.name,workRole.role_salary FROM employee LEFT JOIN workRole ON employee.role_id =workRole.id LEFT JOIN department ON workRole.department_id = department.id';
-    db.query(select,(err,res) => {
+    const select = 'SELECT employee.id,employee.first_name,employee.last_name,workRole.role_name,department.name,workRole.role_salary FROM employee LEFT JOIN workRole ON employee.role_id =workRole.id LEFT JOIN department ON workRole.department_id = department.id';
+    db.query(select, (err, res) => {
         console.table(res);
         if (err) throw err;
         start();
@@ -97,12 +97,70 @@ function viewEmployees() {
 }
 
 // add a department
-
-// add a role
+function addDepartment() {
+    inquirer
+        .prompt({
+            type: "input",
+            name: "addDepartment",
+            message: "What department would you like to add?:"
+        })
+        .then((answer) => {
+            const insert = `INSERT INTO department(name) VALUES ("${answer.addDepartment}")`;
+            db.query(insert, (err, res) => {
+                console.log(`Added ${answer.addDepartment} into departments`);
+                if (err) throw err;
+                start();
+            });
+        });
+}
+// add a role: name of role, salary and department
+function addRole() {
+    const select = "SELECT * FROM department";
+    db.query(select, (err, res) => {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "roleName",
+                    message: "What is the name of the role you would like to add?:",
+                },
+                {
+                    type: "input",
+                    name: "roleSalary",
+                    message: "What is the starting salary for this role?:",
+                },
+                {
+                    type: "list",
+                    name: "roleDepartment",
+                    message: "What department does this role belong to?:",
+                    choices: res.map(
+                        (department) => department.name
+                    ),
+                },
+            ])
+            .then((answer) => {
+                const department = res.find(
+                    (department) => department.name === answer.department
+                );
+                const insert = "INSERT INTO workRole SET ?";
+                db.query(insert,
+                    {
+                        role_name: answer.roleName,
+                        role_salary: answer.roleSalary,
+                        department_id: department,
+                    },
+                    (err,res) => {
+                        console.log (`Added the ${answer.roleName} role into the ${answer.roleDepartment} department`);
+                        if (err) throw err;
+                        start();
+                    }
+                );
+            });
+            if (err) throw err;
+    });
+}
 
 // add an employee
-
-// exit
 
 app.use((req, res) => {
     res.status(404).end();
